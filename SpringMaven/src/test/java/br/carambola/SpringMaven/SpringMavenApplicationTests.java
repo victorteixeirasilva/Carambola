03a21568a95b39cd75821fb8c16419e5ebc5e798
victorteixeirasilva <victorteixeira2308@gmail.com>
2023-04-08 22:32:39 -0300

    SpringMaven

---
+++ SpringMaven/src/test/java/br/carambola/SpringMaven/SpringMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package br.carambola.SpringMaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
