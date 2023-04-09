4c7763e288a19cb1f0f26ebe3728d74f47e34840
victorteixeirasilva <victorteixeira2308@gmail.com>
2023-04-08 22:23:48 -0300

    SpringMavem

---
+++ SpringMavem/src/main/java/br/carambola/SpringMavem/SpringMavemApplication.java
@@ -0,0 +1,13 @@
+package br.carambola.SpringMavem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMavemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMavemApplication.class, args);
+	}
+
+}
