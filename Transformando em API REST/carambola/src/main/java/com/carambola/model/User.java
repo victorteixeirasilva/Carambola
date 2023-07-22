e8910f0e00ab22556bb89ea5449c62d4252dd956
victorteixeirasilva <victorteixeira2308@gmail.com>
2023-07-22 10:39:13 -0300

    PreKickOff

---
+++ Transformando em API REST/carambola/src/main/java/com/carambola/model/User.java
@@ -0,0 +1,11 @@
+package com.carambola.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import org.springframework.stereotype.Component;
+
+@Entity
+@Table(name = "TB_USER")
+@Component
+public class User {
+}
