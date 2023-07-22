c7bc4f73533470d9637881b6cdbf11c09df555b6
victorteixeirasilva <victorteixeira2308@gmail.com>
2023-07-21 23:27:10 -0300

    Preparando dependencias iniciais

---
+++ Transformando em API REST/carambola/src/main/java/com/carambola/CarambolaApplication.java
@@ -0,0 +1,18 @@
+package com.carambola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarambolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarambolaApplication.class, args);
+	}
+
+
+
+
+
+
+}
