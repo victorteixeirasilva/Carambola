fbf9cb844e11ca7a2138ccf8279eb332478b0981
victorteixeirasilva <victorteixeira2308@gmail.com>
2023-07-23 15:36:13 -0300

    1.0

---
+++ Transformando em API REST/carambola/src/main/java/com/carambola/model/Cart.java
@@ -0,0 +1,33 @@
+package com.carambola.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+/**
+ * @description Classe responsavel por administar as mesas quais o usuário vai receber o pedido
+ * e as mesas disponíveis no estabelecimento.
+ *
+ *
+ * @author victorteixeirasilva 23/julho/2023
+ * @version 1.0
+ *
+ * */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Component
+@Entity
+@Table(name = "TB_CART")
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @OneToOne
+    private User user;
+    private long cartNumber;
+    private boolean isAvailable;
+}
