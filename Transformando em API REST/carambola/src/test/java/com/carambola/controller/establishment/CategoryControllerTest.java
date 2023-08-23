c0815e7fdb0a09e6c8584ff202cac4c3db996d4f
victorteixeirasilva <victorteixeira2308@gmail.com>
2023-08-23 12:16:30 -0300

    Update CategoryControllerTest.java

--- Transformando em API REST/carambola/src/test/java/com/carambola/controller/establishment/CategoryControllerTest.java
+++ Transformando em API REST/carambola/src/test/java/com/carambola/controller/establishment/CategoryControllerTest.java
@@ -1,4 +1,20 @@
 package com.carambola.controller.establishment;
 
+import com.carambola.service.CategoryService;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class CategoryControllerTest {
+
+    @Mock
+    private CategoryService categoryService;
+
+    @InjectMocks
+    private CategoryController categoryController;
+
+
+
 }
